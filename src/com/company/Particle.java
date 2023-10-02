b5a7a897c0474bf7f5264f383acc3d4c3bff3c9d
furretcat <przygoda.alex@gmail.com>
2023-10-02 15:24:14 +0200

    n

--- src/com/company/Particle.java
+++ src/com/company/Particle.java
@@ -1,5 +1,10 @@
 package com.company;
 
 public class Particle {
-
+    int x;
+    int y;
+    Direction direction;
+    Sensor sensorL = new Sensor(x+direction.x);
+    Sensor sensorR;
+    Sensor sensorFront = new Sensor(x+ direction.x, y+ direction.y);
 }
---
+++ src/com/company/Sensor.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Sensor {
+    int x;
+    int y;
+    Direction direction;
+
+    public Sensor(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
